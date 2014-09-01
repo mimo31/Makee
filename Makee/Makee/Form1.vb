d82804865567213bfd9e6dbcb1dd0063ac7d8fea
mimo31 <weweuuuu@email.cz>
2014-09-01 14:30:34 +0200

    Flickering Fix

--- Makee/Makee/Form1.vb
+++ Makee/Makee/Form1.vb
@@ -70,6 +70,7 @@
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Initialize.Initialize.Initialize()
+        DoubleBuffered = True
     End Sub
 
     Private Sub Form1_MouseClick(sender As Object, e As MouseEventArgs) Handles MyBase.MouseClick
